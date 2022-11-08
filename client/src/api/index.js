a4ee3f05bf6cabb8134dfa080ac0cc78173ae478
Omer Dasar <omerfarukdasar@gmail.com>
2022-11-08 20:54:08 +0100

    built

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5001/posts";
+const url = "https://memory-api-v5by.onrender.com/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
